ea24d98b4af14110b22b53ec9506c6f2f43dd17e
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-02-23 20:35:46 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const path = require('path');
+const webpack = require('webpack');
+
+
+
+
+/*
+ * We've enabled MiniCssExtractPlugin for you. This allows your app to
+ * use css modules that will be moved into a separate CSS file instead of inside
+ * one of your module entries!
+ *
+ * https://github.com/webpack-contrib/mini-css-extract-plugin
+ *
+ */
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, './dist'),
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080,
+  },
+
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({ filename:'main.[contenthash].css' }),
+    new HtmlWebpackPlugin({
+      title: 'webpack Boilerplate',
+      template: path.resolve(__dirname, './src/template.html'), // шаблон
+      filename: 'index.html', // название выходного файла
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      include: [path.resolve(__dirname, 'src')],
+      loader: 'babel-loader'
+    }, {
+      test: /.(sa|sc|c)ss$/,
+
+      use: [{
+        loader: MiniCssExtractPlugin.loader
+      }, {
+        loader: "css-loader",
+
+        options: {
+          sourceMap: true
+        }
+      }, {
+        loader: "sass-loader",
+
+        options: {
+          sourceMap: true
+        }
+      }]
+    }]
+  }
+}
